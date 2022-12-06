723397fc1de9f6874dae8cfd7818bf385a46b9d3
Ryoma Kai <legknmj@icloud.com>
2022-12-06 11:49:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,28 @@ def tweet(update):
   )
   t.create_tweet(text=update, in_reply_to_tweet_id=reply_id)
 
-twitch = await Twitch(os.environ.get('TWITCH_CLIENT_ID'), os.environ.get('TWITCH_CLIENT_SECRET'))
+  
+async def main():
+  twitch = await Twitch(os.environ.get('TWITCH_CLIENT_ID'), os.environ.get('TWITCH_CLIENT_SECRET'))
 
-# get ID of user
-user_info = twitch.get_users(logins=[os.environ.get('TWITCH_USERNAME')])['data']
-if len(user_info)== 0:
-    print("ERROR: user not found")
-    exit(0)
-user_id = user_info[0]['id']
+  # get ID of user
+  user_info = twitch.get_users(logins=[os.environ.get('TWITCH_USERNAME')])['data']
+  if len(user_info)== 0:
+      print("ERROR: user not found")
+      exit(0)
+  user_id = user_info[0]['id']
 
-# get stream is live
-stream_infos = twitch.get_streams(user_id=[user_id])['data']
-if len(stream_infos) == 0:
-    print("ERROR: user is not streaming")
-    exit(0)
-stream_info = stream_infos[0]
+  # get stream is live
+  stream_infos = twitch.get_streams(user_id=[user_id])['data']
+  if len(stream_infos) == 0:
+      print("ERROR: user is not streaming")
+      exit(0)
+  stream_info = stream_infos[0]
 
-# tweet
-title = stream_info['title']
-url = "https://www.twitch.tv/" + stream_info['user_login']
-hash_tag = "#" + stream_info['game_name'].replace(" ", "")
+  # tweet
+  title = stream_info['title']
+  url = "https://www.twitch.tv/" + stream_info['user_login']
+  hash_tag = "#" + stream_info['game_name'].replace(" ", "")
 
-print("title: %s\ntag: %s\nurl: %s" % (title, hash_tag, url))
-tweet("🔴LIVE: %s\n%s\n%s" % (title, hash_tag, url))
+  print("title: %s\ntag: %s\nurl: %s" % (title, hash_tag, url))
+  tweet("🔴LIVE: %s\n%s\n%s" % (title, hash_tag, url))
